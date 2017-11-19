fc92c841d0abf65114878e502910f5e16923802c
dl8sd11 <j312094@nehs.hc.edu.tw>
2017-11-19 14:18:47 +0800

    892A.cpp

--- codeforces/892A.cpp
+++ codeforces/892A.cpp
@@ -11,8 +11,8 @@ using namespace std;
 #define rall(cont) cont.end(), cont.begin()
 #define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
 #define IN(A, B, C) assert( B <= A && A <= C)
-#define MP make_pair
-#define PB push_back
+#define mp make_pair
+#define pb push_back
 #define INF (int)1e9
 #define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
@@ -40,7 +40,27 @@ void IOS(){
 int main()
 {
   IOS();
-  cout<<"HI"<<endl;
+  ll n;
+  cin>>n;
+  ll sum = 0;
+  REP(i,n) {
+    ll tmp;
+    cin>>tmp;
+    sum += tmp;
+  }
+  vector<ll> v;
+  REP(i,n) {
+    ll tmp;
+    cin>>tmp;
+    v.pb(tmp);
+  }
+  sort(all(v));
+  ll vsize = ll(v.size());
+  if (sum > v[vsize - 1] + v[vsize - 2]) {
+    cout<<"No"<<endl;
+  } else {
+    cout<<"Yes"<<endl;
+  }
 
 	return 0;
 }
