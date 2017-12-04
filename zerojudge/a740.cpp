dd92fce7614ba7da2f122a02dc8728c4e8a34d9a
dl8sd11 <j312094@nehs.hc.edu.tw>
2017-12-04 23:06:08 +0800

    Zerojudge prime

---
+++ zerojudge/a740.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (ll i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define mp make_pair
+#define pb push_back
+#define X first
+#define Y second
+typedef pair<ll, ll> pi;
+#ifdef tmd
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<" ,";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+#else
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll INF = (ll)1e18 + 7;
+const ll MOD = 1000000007;
+const ll n = 5000;
+
+bool sieve[n];
+vector<int> prime;
+void linear_sieve(){
+    for (int i = 2; i < n; i++) {
+      if (!sieve[i]) prime.pb(i);
+      for (int j = 0; i * prime[j] < n; j++) {
+        sieve[i*prime.at(j)] = 1;
+        if (i % prime.at(j) == 0) break;
+      }
+    }
+}
+int main()
+{
+  //20000000 sqrt 4480
+  IOS();
+  sieve[0] = 1;
+  sieve[1] = 1;
+  linear_sieve();
+  int t;
+  while (cin>>t) {
+    ll sum = 0;
+    int idx = 0;
+    while (t != 1 && prime[idx] < 4480) {
+      while (t % prime.at(idx) == 0) {
+        sum += prime[idx];
+        t /= prime[idx];
+      }
+      idx++;
+    }
+    if (t != 1) sum += t;
+    cout<<sum<<endl;
+  }
+	return 0;
+}
---
+++ zerojudge/test.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  bitset<8> bs;
+  bs.set(1);
+  cout<<bs<<endl;
+}
