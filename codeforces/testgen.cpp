00b61d8c3fdb14dbd310828797a9df167c1ab08c
dl8sd11 <j312094@nehs.hc.edu.tw>
2017-12-17 12:56:55 +0800

    add 900 903

--- codeforces/testgen.cpp
+++ codeforces/testgen.cpp
@@ -1,7 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-  
+#define mp make_pair
+#define pb push_back
+#define makeunique(x) sort(all(x)), (x).resize(unique(all(x)) - (x).begin())
+#define re return
+#define fi first
+#define se second
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define sqrt(x) sqrt(abs(x))
+#define y0 y3487465
+#define y1 y8687969
+#define j0 j5743892
+#define j1 j542893
 
+typedef vector<int> vi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef double D;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+
+template<class T> T abs(T x) { re x > 0 ? x : -x; }
+template<class T> T gcd(T a, T b) { re a ? gcd (b % a, a) : b; }
+template<class T> T sqr(T a) { re a * a; }
+template<class T> T sgn(T a) { re a > 0 ? 1 : (a < 0 ? -1 : 0); }
+
+#define filename "test"
+
+
+
+
+int main () {
+	int n = 2, k = 2500;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<k;j++){
+      cout<< char((j+i)%26+'a');
+    }
+    cout<<endl;
+  }
+	return 0;
 }
