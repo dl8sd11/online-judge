ba6e33c7c8a5dd5e3a8099674e3ec61fb4801145
dl8sd11 <tmd910607@gmail.com>
2018-01-10 15:55:17 +0000

    TLE

---
+++ SproutOJ/p159.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (ll i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define mp make_pair
+#define pb push_back
+#define X first
+#define Y second
+typedef pair<ll, ll> pi;
+#ifdef tmd
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<" ,";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+#else
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll INF = (ll)1e18 + 7;
+const ll MOD = 1000000007;
+const ll MAXN = int(1e2) + 3;
+int t;
+int n,m;
+int c[MAXN],k[MAXN];
+bool dp[2][20003];
+deque<int> dq[203];
+int main()
+{
+  IOS();
+  cin>>t;
+  while (t--) {
+    MEM(dp,0);
+    cin>>n>>m;
+    bool roll = 1;
+    dp[1][0] = 1;
+    REP (i,n) cin>>c[i]>>k[i];
+    REP (i,n) {
+      roll = !roll;
+      REP(j,203) dq[j].clear();
+      REP (j,m+1) {
+        int idx = j%c[i];
+        if (dp[!roll][j]) {
+          //debug(mp(i,j));
+          dp[roll][j] = 1;
+          dq[idx].pb(j);
+        }
+
+        while (dq[idx].size()&&dq[idx].front()<j-c[i]*k[i]) {
+          dq[idx].pop_front();
+        }
+        if (dq[idx].size()) {
+          dp[roll][j] = 1;
+        }
+      }
+
+    }
+    if (dp[roll][m]) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+  }
+	return 0;
+}
---
+++ tmp/pB.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (ll i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define mp make_pair
+#define pb push_back
+#define X first
+#define Y second
+typedef pair<ll, ll> pi;
+#ifdef tmd
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<" ,";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+#else
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll INF = (ll)1e18 + 7;
+const ll MOD = 1000000007;
+ll t,y;
+string a,tmp;
+vector<string> o(4);
+/********** Main()  function **********/
+void solve() {
+  tmp = "";
+  int idx = 0;
+  REP (i,ll(a.size())) {
+    if (a[i]!=','){
+      tmp+=a[i];
+    }
+    else {
+      o[idx++] = tmp;
+      tmp = "";
+    }
+  }
+  o[idx] = tmp;
+}
+
+int main()
+{
+  IOS();
+  cin>>t;
+  cin.ignore();
+  cin>>y;
+  cin.ignore();
+  while (t--) {
+    getline(cin,a);
+    solve();
+    cout<<"Name: "<<o[0]<<endl;
+    transform(o[1].begin(),o[1].end(),o[1].begin(),::tolower);
+    cout<<"Account: "<<o[1]<<endl;
+    if (o[2].size()==2) cout<<"Class: "<<o[2]<<endl;
+    else cout<<"Class: 0"<<o[2]<<endl;
+    cout<<"Age: "<<y-atoi(o[3].c_str())<<endl;
+    if (t) cout<<endl;
+  }
+
+
+	return 0;
+}
