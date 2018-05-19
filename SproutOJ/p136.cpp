47970f4dd8d4650c06c835b6ceb1c0121540234b
dl8sd11 <tmd910607@gmail.com>
2018-05-20 00:55:02 +0800

    20180519 in Sprout

--- SproutOJ/p136.cpp
+++ SproutOJ/p136.cpp
@@ -12,34 +12,11 @@ void Report(int id) {
 struct nd{
   ll sum;
   vector<ll> nel;
+  nd(){sum=0;}
 };
 vector<nd> la,ra;
 vector<ll> el;
-void dfsa(ll A[],ll st,ll idx,ll ed,ll sum){
-  if(idx>=ed){
-    nd tmp = {sum,el};
-    la.push_back(tmp);
-    return;
-  }
-  dfsa(A,st,idx+1,ed,sum);
-  el.push_back(A[idx]);
-  dfsa(A,st,idx+1,ed,sum+A[idx]);
-  if(el.empty())while(1){}
-  el.pop_back();
-}
-void dfsb(ll A[],ll st,ll idx,ll ed,ll sum){
-  if(idx>=ed){
-    nd tmp = {sum,el};
-    ra.push_back(tmp);
 
-    return;
-  }
-  dfsb(A,st,idx+1,ed,sum);
-  el.push_back(A[idx]);
-  dfsb(A,st,idx+1,ed,sum+A[idx]);
-  if(el.empty())while(1){}
-  el.pop_back();
-}
 void solve(int N,ll K,ll A[]){
   la.clear();
   ra.clear();
@@ -50,18 +27,40 @@ void solve(int N,ll K,ll A[]){
     }
     return;
   }
-  el.clear();
-  dfsa(A,0,0,N/2,0);
-  el.clear();
-  dfsb(A,N/2,N/2,N,0);
+
+
+  for(ll i=0;i<(1<<N/2);i++){
+    nd tmp;
+    for(ll j=0;j<N/2;j++){
+      if(1<<j&i){
+        tmp.sum+=A[j];
+        tmp.nel.push_back(A[j]);
+      }
+    }
+    la.push_back(tmp);
+  }
+
+  ll rsz = N - N/2;
+  for(ll i=0;i<(1<<rsz);i++){
+    nd tmp;
+    for(ll j=0;j<rsz;j++){
+      if(1<<j&i){
+        tmp.sum+=A[j+N/2];
+        tmp.nel.push_back(A[j+N/2]);
+      }
+    }
+    ra.push_back(tmp);
+  }
+  // while(1){}
   sort(la.begin(),la.end(),[](const nd &a,const nd &b){return a.sum<b.sum;});
   sort(ra.begin(),ra.end(),[](const nd &a,const nd &b){return a.sum<b.sum;});
   for (nd x: la){
+
       ll tg = K - x.sum;
       if(tg==0){
         for(auto _el:x.nel)Report(_el);
         Report(-1);
-        return;
+        break;
       }
       ll l=0,r=ra.size();
       while(r-l>1){
@@ -73,7 +72,7 @@ void solve(int N,ll K,ll A[]){
         for(auto _el:x.nel)Report(_el);
         for(auto _el:ra[l].nel)Report(_el);
         Report(-1);
-        return;
+        break;
       }
   }
   Report(-1);
@@ -82,8 +81,8 @@ void solve(int N,ll K,ll A[]){
 int main() {
   // You may change it to any data that you'd like to test with.
   int N = 40;
-  int K = 20968761;
-  long long A[] = {289383,930886,692777,636915,747793,238335,885386,760492,516649,641421,202362,490027,368690,520059,897763,513926,180540,383426,89172,455736,5211,595368,702567,956429,465782,21530,722862,665123,174067,703135,513929,979802,634022,723058,133069,898167,961393,18456,175011,478042};
+  int K = 20679378;
+  long long A[] = {930886,692777,636915,747793,238335,885386,760492,516649,641421,202362,490027,368690,520059,897763,513926,180540,383426,89172,455736,5211,595368,702567,956429,465782,21530,722862,665123,174067,703135,513929,979802,634022,723058,133069,898167,961393,18456,175011,478042};
   solve(N, K, A);
   return 0;
 }
