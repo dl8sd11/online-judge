da89d368f879530ecf6039782ae503b89b42a7a8
dl8sd11 <tmd910607@gmail.com>
2019-08-06 11:37:42 +0800

    Fake Segment Tree Problem

---
+++ SPOJ/POSTERS.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<double,double> pdd;
+#define SQ(i) ((i)*(i))
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define SZ(i) int(i.size())
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define X first
+#define Y second
+#ifdef tmd
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<" ,";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+#else
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll MOD = 1000000007;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int iNF = 0x3f3f3f3f;
+// const ll MAXN = 
+
+int t, n;
+bool app[40004];
+set<int> pst;
+vector<pii> event;
+/********** Good Luck :) **********/
+int main()
+{
+    IOS();
+    cin >> t;
+    while (t--) {
+        event.clear();
+        pst.clear();
+        cin >> n;
+        REP1 (i, n) {
+            int l, r;
+            cin >> l >> r;
+            event.eb(l, -i);
+            event.eb(r, i);
+        }
+        memset(app, 0, (n+1) * sizeof(bool));
+
+        sort(ALL(event));
+        REP (i, SZ(event)) {
+            int hd = i;
+            while (i < SZ(event) && event[i].X == event[hd].X && event[i].Y < 0) {
+                pst.insert(-event[i].Y);
+                i++;
+            }
+
+            app[*prev(pst.end())] = true;
+
+            while (i < SZ(event) && event[i].X == event[hd].X) {
+                assert(event[i].Y > 0);
+                pst.erase(event[i].Y);
+                i++;
+            }
+            i--;
+        }
+
+        int ans = 0;
+        REP1 (i, n) {
+            ans += app[i];
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
