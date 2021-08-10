dd37723cf1173563e8a1abac0efe6fd254e774d1
dl8sd11 <tmd910607@gmail.com>
2021-08-10 21:07:58 +0800

    remove trash

--- test/tester-cpp.sh
+++ test/tester-cpp.sh
@@ -1,10 +1,9 @@
 set -e
 g++ -std=c++17 me.cpp -o me.out
 g++ -std=c++17 ans.cpp -o ans.out
-g++ -O2 testgen.cpp -o testgen
 while [ 1 ]
 do
-    ./testgen 100 100 > testcase.in
+    ./gen.out 5 5 5 10 > testcase.in
     ./me.out < testcase.in > a1.out
     ./ans.out < testcase.in > a2.out
     diff a1.out a2.out
--- test/tester.txt
+++
@@ -1,9 +0,0 @@
-set -e
-while [ 1 ]
-do
-    ./testgen.py > testcase.in
-    ./rectME.out < testcase.in > a1.out
-    ./rectANS.out < testcase.in > a2.out
-    diff a1.out a2.out
-    echo "Accept"
-done
